5012271edf9e5b4e35da7e483f9817ba23fc518e
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-08-23 02:40:25 +0530

    ShowCustomeDataFaces added

--- Modules/utils.py
+++ Modules/utils.py
@@ -40,3 +40,38 @@ def ShowMissclassifiedImages(model, data, class_id, device,dataType='val', num_i
                       fig.savefig(save_as)
                       return
         model.train(mode=was_training)
+
+def ShowCustomDataFaces(model, data, class_id, device,dataType='val', num_images=12,save_as="misclassified.jpg"):
+    dataloaders, class_names = data.dataloaders, data.class_names
+    was_training = model.training
+    model.eval()
+    images_so_far = 5
+    fig, axs = plt.subplots(int(num_images/4),4,figsize=(12,12))
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders[dataType]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+              
+            for j in range(inputs.size()[0]):
+                row = int((images_so_far)/4)
+                col = (images_so_far)%4
+                imagex = inputs.cpu().data[j]
+                imagex = np.transpose(imagex, (1, 2, 0))
+                imagex=imagex.numpy()
+                mean = np.array([0.53713346, 0.58979464, 0.62127595])
+                std = np.array([0.27420551, 0.25534403, 0.29759673])
+                imagex = std*imagex  + mean
+                imagex = np.clip(imagex, 0, 1)       
+                axs[row,col].imshow(imagex)
+                axs[row,col].axis('off')
+                fig.tight_layout(pad=2.0)
+                axs[row,col].set_title('Predicted: {} \n Actual: {}'.format(class_names[preds[j]],class_names[labels[j]]))
+                images_so_far += 1
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    plt.show()
+                    fig.savefig(save_as)
+                    return
+        model.train(mode=was_training)
