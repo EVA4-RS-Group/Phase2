e2470d9d765ac6dac5cb11ba1d84b19bbd266c34
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-09-02 20:08:12 +0530

    color added to plots

--- S5_HumanPoseEstimation/src/inference.py
+++ S5_HumanPoseEstimation/src/inference.py
@@ -41,6 +41,25 @@ POSE_PAIRS = [
             [14, 15]
 ]
 
+POSE_PAIRS_COL = [
+    (113,191,66),
+    (237,56,51),
+    (114,192,67),
+    (157,74,182),
+    (253,226,107),
+    (41,125,194),
+    (41,126,195),
+    (157,74,182),
+    (113,191,66),
+    (237,56,51),
+    (114,192,67),
+    (157,74,182),
+    (253,226,107),
+    (41,125,194),
+    (157,74,182),
+    (113,191,66)
+]
+
 class HPEInference():
     """ Docstring
     """
@@ -106,13 +125,15 @@ class HPEInference():
         key_points = list(get_keypoints(pose_layers=pose_layers))
         key_points = [(thres,(int(x*scale_x),int(y*scale_y))) for thres,(x,y) in key_points]
 
+        i=0
         for from_j, to_j in POSE_PAIRS:
             from_thr, (from_x_j, from_y_j) = key_points[from_j]
             to_thr, (to_x_j, to_y_j) = key_points[to_j]
 
             if from_thr > THRESHOLD and to_thr > THRESHOLD:
                 # this is a joint connection, plot a line
-                cv2.line(image_p, (from_x_j, from_y_j), (to_x_j, to_y_j), (255, 74, 0), 3)
+                cv2.line(image_p, (from_x_j, from_y_j), (to_x_j, to_y_j), POSE_PAIRS_COL[i], 3)
+            i+=1
 
         for thres,(x,y) in key_points:
             if thres > THRESHOLD:
